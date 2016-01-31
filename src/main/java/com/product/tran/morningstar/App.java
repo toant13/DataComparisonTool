e766e809cbdb91a8583eee7636de80fdf55bd63c
Tony Tran <toant713@gmail.com>
2016-01-30 21:44:23 -0500

    added initial logic for file comparisons

--- src/main/java/com/product/tran/morningstar/App.java
+++ src/main/java/com/product/tran/morningstar/App.java
@@ -2,4 +2,13 @@ package com.product.tran.morningstar;
 
 public class App {
 
+	public static void main(String[] args) throws Exception {
+		String productFile = "/Users/toantran/Documents/Java_development/workspace/DataComparisonTool/src/test/resources/InputFiles/product.csv";
+		String morningStarFile = "/Users/toantran/Documents/Java_development/workspace/DataComparisonTool/src/test/resources/InputFiles/morningstar.csv";
+		String mapFile = "/Users/toantran/Documents/Java_development/workspace/DataComparisonTool/src/test/resources/map/map.txt";
+
+		MorningStarComparer comparer = new MorningStarComparer(productFile,
+				morningStarFile, mapFile);
+		comparer.runCompare();
+	}
 }
--- src/main/java/com/product/tran/morningstar/MorningStarComparer.java
+++ src/main/java/com/product/tran/morningstar/MorningStarComparer.java
@@ -1,28 +1,46 @@
 package com.product.tran.morningstar;
 
+import java.util.List;
+import java.util.Map;
+
 import com.product.tran.common.load.DataLoader;
 import com.product.tran.common.load.MapLoader;
 
+/**
+ * @author toantran
+ *
+ */
 public class MorningStarComparer {
-	
+
 	private MapLoader mapLoader;
 	private DataLoader productLoader;
 	private DataLoader morningStarLoader;
 
-	public MorningStarComparer(String productFile, String morningStarFile, String mapFile) throws Exception{
+	public MorningStarComparer(String productFile, String morningStarFile,
+			String mapFile) throws Exception {
 		this.mapLoader = new MapLoader();
 		mapLoader.load(mapFile);
-		
+
 		this.productLoader = new DataLoader();
 		productLoader.load(productFile);
-		
+
 		this.morningStarLoader = new DataLoader();
 		morningStarLoader.load(morningStarFile);
 	}
-	
-	public void runCompare() throws Exception{
+
+	public void runCompare() throws Exception {
+		Map<String, String> columnMapper = mapLoader.getColumnMapper();
+		Map<String, String> typeMapper = mapLoader.getTypeMapper();
 		
+		Map<String, List<String>> productValuesMap = productLoader.getColumnValuesMap();
+		Map<String, List<String>> morningStarValuesMap = morningStarLoader.getColumnValuesMap();
 		
-		
+		for(Map.Entry<String, String> column : columnMapper.entrySet()){
+			
+		}
 	}
+
+
+
+
 }
