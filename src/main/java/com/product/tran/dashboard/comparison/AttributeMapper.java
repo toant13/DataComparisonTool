9da2ea4f9a96937dd56bd26b63ad2832bee90f42
Tony Tran <toant713@gmail.com>
2016-03-28 22:03:59 -0400

    creating class to get required columns for comparison base on a keymap

---
+++ src/main/java/com/product/tran/dashboard/comparison/AttributeMapper.java
@@ -0,0 +1,12 @@
+package com.product.tran.dashboard.comparison;
+
+/**
+ * returns columns required for comparison
+ * 
+ * 
+ * @author toantran
+ *
+ */
+public class AttributeMapper {
+
+}
--- src/main/java/com/product/tran/dashboard/comparison/Comparer.java
+++ src/main/java/com/product/tran/dashboard/comparison/Comparer.java
@@ -1,12 +1,30 @@
 package com.product.tran.dashboard.comparison;
 
+/**
+ * Abstract class to define comparers.
+ * 
+ * @author toantran
+ *
+ */
 public abstract class Comparer {
 
 	protected int threshold;
 	
+	/**
+	 * Constructor with threshold for compares
+	 * 
+	 * @param threshold
+	 */
 	public Comparer(int threshold){
 		this.threshold = threshold;
 	}
 
+	/**
+	 * Checks data using required threshold
+	 * 
+	 * @param value1
+	 * @param value2
+	 * @return
+	 */
 	public abstract boolean checkData(String value1, String value2);
 }
